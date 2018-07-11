a8df9ee84475aef4994e249d6106bb022eb34a18
heixiongTT <386743836@qq.com>
2018-07-11 17:25:11 +0800

    Update main.go

--- simple/main.go
+++ simple/main.go
@@ -226,6 +226,63 @@ func (t *Chaincode) readMultiSegData(stub shim.ChaincodeStubInterface, key strin
 	return shim.Success(ret)
 }
 
+func (t *SimpleChaincode) queryByParam(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	
+	if len(args) < 1 {
+		return shim.Error("Incorrect number of arguments. Expecting 1")
+	}
+
+	queryString := args[0]
+
+	queryResults, err := getQueryResultForQueryString(stub, queryString)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	return shim.Success(queryResults)
+}
+
+func getQueryResultForQueryString(stub shim.ChaincodeStubInterface, queryString string) ([]byte, error) {
+
+	fmt.Printf("- getQueryResultForQueryString queryString:\n%s\n", queryString)
+
+	resultsIterator, err := stub.GetQueryResult(queryString)
+	if err != nil {
+		return nil, err
+	}
+	defer resultsIterator.Close()
+
+	// buffer is a JSON array containing QueryRecords
+	var buffer bytes.Buffer
+	buffer.WriteString("[")
+
+	bArrayMemberAlreadyWritten := false
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return nil, err
+		}
+		// Add a comma before array members, suppress it for the first array member
+		if bArrayMemberAlreadyWritten == true {
+			buffer.WriteString(",")
+		}
+		buffer.WriteString("{\"Key\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(queryResponse.Key)
+		buffer.WriteString("\"")
+
+		buffer.WriteString(", \"Record\":")
+		// Record is a JSON object, so we write as-is
+		buffer.WriteString(string(queryResponse.Value))
+		buffer.WriteString("}")
+		bArrayMemberAlreadyWritten = true
+	}
+	buffer.WriteString("]")
+
+	fmt.Printf("- getQueryResultForQueryString queryResult:\n%s\n", buffer.String())
+
+	return buffer.Bytes(), nil
+}
+
 //BlockHead descr
 type BlockHead struct {
 	CryptoDescriptor string `json:"cryptoDescriptor"`
@@ -265,6 +322,11 @@ func (t *Chaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 			return shim.Error("parametes's number is wrong")
 		}
 		return t.query(stub, args[0])
+	case "queryByParam": //查询
+		if len(args) != 1 {
+			return shim.Error("parametes's number is wrong")
+		}
+		return t.queryByParam(stub, args)
 	case "history": //查询
 		if len(args) != 1 {
 			return shim.Error("parametes's number is wrong")
